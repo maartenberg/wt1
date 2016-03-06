2278aaf9811abb4269d80c1cde629d40b59e99e8
Maarten van den Berg <maartenberg1+git@gmail.com>
2016-03-06 17:50:44 +0100

    Work work work
    jaja

--- js/performance-charts.js
+++ js/performance-charts.js
@@ -1,5 +1,19 @@
 // This file contains the code to draw the charts included in the Performance section.
 
+var commonChartData = {
+		series: {
+			lines: { show: false },
+			points: { show: true }
+		},
+		xaxis: {
+			ticks: 20,
+			tickDecimals: 0,
+		},
+		legend: {
+			position: "nw"
+		}
+	};
+
 // Draws the chart comparing connection:close and connection:keep-alive
 function plot_keepalive() {
 	var close_points = [];
@@ -16,18 +30,29 @@ function plot_keepalive() {
 	$.plot("#keepalive_chart", [
 			{ label: "close", hoverable: true, data: close_points}, 
 			{ label: "keep-alive", hoverable:true, data: keepalive_points}
-	], {
-		series: {
-			lines: { show: false },
-			points: { show: true }
-		},
-		xaxis: {
-			ticks: 20,
-			tickDecimals: 0,
-		}
-	});
+	], commonChartData);
+}
+
+function plot_https_keepalive() {
+	var close_points = []; //with connection:close
+	var keepalive_points = []; //with keep-alive
+	
+	for (var i = 1; i <= 20; i += 1) {
+		// close: 6 RTT (3 TCP + 2 TLS + request) * n
+		close_points.push([i, 6*i]);
+
+		// keep-alive: 5RTT + n
+		keepalive_points.push([i, 5 + i]);
+	}
+
+	$.plot("#keepalive_https_chart", [
+			{ label: "close", data: close_points},
+			{ label: "keep-alive", data: keepalive_points }
+			], commonChartData);
+
 }
 $(document).ready(
 	function() {
 		plot_keepalive();
+		plot_https_keepalive();
 	});
