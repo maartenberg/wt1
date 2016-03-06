8d2b54aa2850aae875751ce857b83c142c264224
Machteld <m.hamers97@gmail.com>
2016-03-06 23:20:48 +0100

    Photobrowser pages
    Now multiple pages, prev and next buttons, also javascript sucks.
    JSON info contains amount of pages, start at page 1. Can't go past
    maxpages or below page 1 jej fun ugh

--- photos.js
+++ photos.js
@@ -1,7 +1,9 @@
+var pagenr;
+var maxpages;
 
-(function() {
+function photos() {
 
-    var apiURL = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=88b38fa938012279362204219265a900&photoset_id=72157664952757730&user_id=139085922%40N04&per_page=20&page=1&format=json&jsoncallback=?";
+    var apiURL = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=88b38fa938012279362204219265a900&photoset_id=72157664952757730&user_id=139085922%40N04&per_page=20&format=json&jsoncallback=?&page="+pagenr;
 
     $.getJSON(apiURL, function(data){
 
@@ -10,6 +12,8 @@
         	var img_src = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "m.jpg";
 
         	var a_href = "http://www.flickr.com/photos/" + data.photoset.owner + "/" + photo.id + "/";
+
+        	maxpages = data.photoset.pages;
 
         	/*
 			adds a part to html body:
@@ -20,15 +24,38 @@
 
 			for every image.
         	*/
-
-        	$( "<div class=\"imgthingie\" id=\"" + i + "\">" ).appendTo( "#images")
+        	
+        	$( "<div class=\"imgthingie\" id=\"" + i + "\"> " ).appendTo( "#images").wrap(("<a href='" + a_href + "'></a>"))
         	$("<img/>").attr("src", img_src).appendTo("#"+i)
         	$( "</div>").appendTo("#images")
 
-
         	//WRAP IN LINK
-        	.wrap(("<a href='" + a_href + "'></a>"))
+
       	});
 
     });
-})();
+}
+
+function clickedNext() {
+
+	if (pagenr < maxpages) {
+		$(".images").empty();
+		pagenr++;
+		photos(pagenr);
+	}
+}
+
+function clickedPrev() {
+	if (pagenr > 1) {
+		$(".images").empty();
+		pagenr--;
+		photos();
+	}
+}
+
+function start() {
+	pagenr = 1;
+	photos();
+}
+
+start();
