e8a33756911cc973910e5c754534ff442881c232
Machteld <m.hamers97@gmail.com>
2016-03-07 09:59:02 +0100

    Performance updated refs
    jaja

--- references.js
+++ references.js
@@ -5,14 +5,16 @@ $(document).ready(function() {
 	$("a[title]").addClass("footnote");
 	$(".footnote").each(function() {
 		var id = 'note' + footnote;
+		var uid = 'u' + id;
 		$(this).attr('id', id);
+		$(this).attr('href', '#' + uid)
 		$(this).append("<sup>"+footnote+"</sup>");
 		cite="<li>";
 		url=$(this).attr("data-external");
 		title=$(this).attr("title");
 
 		
-			cite+="<a href=\""+url+"\">"+title+"</a>";
+			cite+="<a href=\""+url+"\" id=\""+uid+"\">"+title+"</a>";
 			cite+="<a href=\"#"+id+"\">&#8617;</a>";
 		
 
